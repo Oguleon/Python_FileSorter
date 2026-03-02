a5ec5e45efdad1211cf5cf381fa551546ce8adbd
Ogulcan Kuecuek <ogulcankucuk05@gmail.com>
2026-03-02 13:52:32 +0100

    Code now affects the 'Downloads'-Folder, removed unnecessary 'i' declaration

--- FileSorter.py
+++ FileSorter.py
@@ -1,29 +1,38 @@
 import os
 import shutil
 
+user_folder = os.path.expanduser("~")
+downloads_folder = os.path.join(user_folder, "Downloads")
+
 # Move File
 def moveFile(fullFilename, dst_folder):
-    src = "./Sandbox/" + fullFilename
-    dst = "./Sandbox/" + dst_folder + "/" + fullFilename
+    src = os.path.join(downloads_folder, fullFilename)
+    dst = os.path.join(downloads_folder, dst_folder, fullFilename)
     shutil.move(src, dst)
 
 # Iterate Filename-List to create and sort into Folders
 def createSortIntoFolders():
-    i = 0
     for fullFilename in fileList:
         filename, filetype = os.path.splitext(fullFilename)
         filetype = filetype.replace(".", "")
-        if os.path.exists("./Sandbox/" + filetype):
+
+        # If folder -> skip
+        if filetype == "":
+            continue
+
+        dst_path = os.path.join(downloads_folder, filetype)
+
+        if os.path.exists(dst_path):
             moveFile(fullFilename, filetype) 
         else:
-            os.makedirs("./Sandbox/" + filetype)
+            os.makedirs(dst_path)
             moveFile(fullFilename, filetype)    
      
 # Main
 print("-====+====-")
 
 # Get Filenames and print them
-fileList = os.listdir("./Sandbox")
+fileList = os.listdir(downloads_folder)
 print("Files: ", fileList)
 createSortIntoFolders()
 
